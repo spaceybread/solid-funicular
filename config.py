75a723d5aacb48a15f38d1266f182e92151642c7
spaceybread <Spaceybread@gmail.com>
2024-03-25 16:10:39 -0500

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ import numpy as np
 #dx, dy: intial velocities in directions x and y
 
 
-# 2 smaller objects orbit a larger, heavier object in the middle
+# config 1: 2 smaller objects orbit a larger, heavier object in the middle
 m = np.array([1, 10, 1])
 x = np.array([-1, 0.0, 1])
 y = np.array([0, 0.0, 0])
@@ -20,9 +20,23 @@ vx = np.array([0, 0, 0])
 vy = np.array([-3.5, 0, 3.5])
 
 
-#the values are based on https://arxiv.org/pdf/math/0011268.pdf for the stable 3 body 8 shape
+#config 2: the values are based on https://arxiv.org/pdf/math/0011268.pdf for the stable 3 body 8 shape
 #m = np.array([1, 1, 1])
 #x = np.array([-0.97000436, 0.0, 0.97000436])
 #y = np.array([0.24208753, 0.0, -0.24208753])
 #vx = np.array([0.4662036850, -0.933240737, 0.4662036850])
 #vy = np.array([0.4323657300, -0.86473146, 0.4323657300])
+
+# config 3: 
+#m = np.array([1, 10, 1])
+#x = np.array([-1, 0.0, 1])
+#y = np.array([0, 0.0, 0])
+#vx = np.array([0, 0, 0])
+#vy = np.array([-3.53, 0, 3.47])
+
+#config 4: 
+#m = np.array([3, 20, 1])
+#x = np.array([-5, 0.0, -4])
+#y = np.array([0, 0.0, 0])
+#vx = np.array([0, 0, 0])
+#vy = np.array([2.5, 0, 0.5])
