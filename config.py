324a03ff0bbd2c79ebeaa38c0c118252e57fdc82
spaceybread <Spaceybread@gmail.com>
2024-03-25 04:06:11 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# 2 smaller objects orbit a larger, heavier object in the middle
+m = np.array([1, 10, 1])
+x = np.array([-1, 0.0, 1])
+y = np.array([0, 0.0, 0])
+vx = np.array([0, 0, 0])
+vy = np.array([-3.5, 0, 3.5])
