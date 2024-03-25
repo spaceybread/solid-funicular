a93d738144de8c62e78da66dcdbe350e7c3c4d7b
spaceybread <Spaceybread@gmail.com>
2024-03-25 04:14:03 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,17 @@
 import numpy as np
 
+#template
+#m = np.array([mass of object 1, mass of object 2, mass of object 3])
+#x = np.array([initial x of object 1, initial x of object 2, initial x of object 3])
+#y = np.array([initial y of object 1, initial y of object 2, initial y of object 3])
+#vx = np.array([initial dx of object 1, initial dx of object 2, initial dx of object 3])
+#vy = np.array([initial dy of object 1, initial dy of object 2, initial dy of object 3])
+
+#m: mass
+#x, y: initial x, y coordinates
+#dx, dy: intial velocities in directions x and y
+
+
 # 2 smaller objects orbit a larger, heavier object in the middle
 m = np.array([1, 10, 1])
 x = np.array([-1, 0.0, 1])
